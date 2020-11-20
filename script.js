039c6d663c2d062701ab7f2a66c3516fe82dcc69
Jorge Trejo <jtrejo@mail.com>
2020-11-19 21:22:15 -0800

    added comments

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ function movieList(e){
 
 //THIS FUNCTION QUERIES THE MOVIE API FOR THE SPECIFIC MOVIE TITLE AND YEAR OF THE POSTER THAT IS CLICKED
 //IT ALSO UNHIDES THE MODAL WITH THE NEWS INFORMATION
+//IT ALSO CALLES THE FUNCTIONI THAT SAVES AND RETRIEVES PAST SEARCHES
 function getMovieInfo(event){
     //Queries the movie API+++++++++++++++++++++++++++++++++++
     var movieYear = $(event.target).attr("data-year")
@@ -119,25 +120,34 @@ function getMovieInfo(event){
     saveSearch(movieName, movieYear)
 }
 
-//======================================================================
+//THIS FUNCTION  SAVES AND RETRIEVES PAST SEARCHES AND ADDS THEM TO A DROPDOWN MENU IN THE MAIN WINDOW
+//IT ALSO STORES THE INFORAMTION IN LOCAL STORAGE
+//IT ALSO CREATES AND APPENDS THE ELEMENTS TO DISPLAY THE INFORMATION
 function saveSearch(movie, year){
+    
     doubleSignal = 0
     var info = {title:movie, year:year}
+    //Checks to see if there is any information in local storage and if there is it retrieves it ++++++++++++++++
     if (localStorage.getItem("pastSearches")){
         pSearches = JSON.parse(localStorage.getItem("pastSearches"))
     }
+    //Checks the array of saved movies to see if the new movie being searched i salready saved and if it is, then it sets a variable as a 
+    //signal to prevent it from being added to the svaed movies array+++++++++++++++++++++++
     pSearches.forEach(em=>{
         if(em.title === movie && em.year === year){
             doubleSignal = 1
         }
     })
+    //Checks if the incoming movie has valid data or if the movie is already saved and prevents it from being saved to the array++++++
     if(info.title === 1 && info.year === 2|| doubleSignal === 1){}
     else{
         pSearches.push(info)   
     }
+    //Saves the "past searches" aray into local storage+++++++++++++
     localStorage.setItem("pastSearches", JSON.stringify(pSearches))
-    // console.log(pSearches)
+    //Clears the menu to prevent repeted lists++++++++++++++++
     dropmenu.children().remove()
+    //Creates the elements that render the list of past searches++++++++++++++
     pSearches.forEach(element=>{
         
         var savelist = $("<p>") 
@@ -147,8 +157,8 @@ function saveSearch(movie, year){
         savelist.append(element.title + " " + element.year)
         dropmenu.append(savelist) 
     })
+}
     
-    // console.log(movie)
 }
 
 //THIS FUNCTIONCONVERTS THE DATE OBTAINED FROM THE MOVIE API QUERY TO A SUITABLE FORMAT FOR THE NYT QUERY
@@ -344,12 +354,14 @@ document.addEventListener("click", function(event){
 modalCloseButton.on("click", function(event){
     modalMainEl.attr("style", "display:none")
 })
-
+//THIS EVENT LISTENER UN-HIDES THE "PAST SEARCHES" LIST AND CALLS THE FUNCTION TO RENDER THE LIST
 pSearchesButton.on("click", function(event){
     dropmenu.attr("style", "display:flex" );
     saveSearch(1, 2)
 })
 
+//THIS EVENT LISTENER CLOSES THE "PAST SEARCHES" LIST WHENEVER THE MOUSE CURSOR IS MOVED AWAY FROM THE LIST OF SEARCHES OR 
+//THE "PAST SEARCHES"BUTTON
 window.onmouseover = function(event){
     if(event.target.id === "dropbttn" || event.target.tagName === "P"){
         return
@@ -359,9 +371,10 @@ window.onmouseover = function(event){
     }
 }
 
+//THIS EVENT LISTENER CALLS THE FUNCTION THAT DISPLAYS THE MODAL AND QUERIES THE NYT API WHENEVER ONE OF THE ITEMS IN THE 
+//"PAST SEARCH" LIST IS CLICKED
 document.addEventListener("click", function(event){
     if (event.target.className === "saves"){
-        console.log(event.target)
         getMovieInfo(event)
     }
     else{
